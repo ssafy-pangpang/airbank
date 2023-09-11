472ef28dfd3a46fd3757fc14d3acef7dd62db4d7
jjuny7712 <jjuny7712@gmail.com>
2023-09-11 13:05:49 +0900

    init: 초기 프로젝트 생성

---
+++ airbank-be/src/main/java/com/pangpang/airbank/AirbankApplication.java
@@ -0,0 +1,13 @@
+package com.pangpang.airbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbankApplication.class, args);
+	}
+
+}
---
+++ airbank-be/src/test/java/com/pangpang/airbank/AirbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pangpang.airbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
