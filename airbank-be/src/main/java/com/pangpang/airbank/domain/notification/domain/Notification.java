4531f96239d13b7642cab41e1af948c8be683b6b
Chae Huichan <eqholic1125@gmail.com>
2023-09-19 09:54:59 +0900

    refactor: Notification Document 내용 및 구조 변경

--- airbank-be/src/main/java/com/pangpang/airbank/domain/notification/domain/Notification.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/notification/domain/Notification.java
@@ -1,62 +1,63 @@
 package com.pangpang.airbank.domain.notification.domain;
 
-import org.hibernate.annotations.ColumnDefault;
+import java.time.LocalDateTime;
 
-import com.pangpang.airbank.domain.BaseTimeEntity;
-import com.pangpang.airbank.domain.member.domain.Member;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.pangpang.airbank.domain.notification.dto.CreateNotificationDto;
 import com.pangpang.airbank.global.meta.converter.NotificationTypeConverter;
 import com.pangpang.airbank.global.meta.domain.NotificationType;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "notification")
+@Document(collection = "notification")
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Notification extends BaseTimeEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class Notification {
 
-	@Size(max = 255)
 	@NotNull
-	@Column
 	private String content;
 
 	@NotNull
 	@Builder.Default
-	@ColumnDefault("false")
-	@Column
 	private Boolean activated = Boolean.FALSE;
 
 	@NotNull
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "sender_id", foreignKey = @ForeignKey(name = "fk_notification_to_member_sender_id"))
-	private Member sender;
+	private Long senderId;
 
 	@NotNull
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "receiver_id", foreignKey = @ForeignKey(name = "fk_notification_to_member_receiver_id"))
-	private Member receiver;
+	private Long receiverId;
 
 	@NotNull
-	@Column(length = 20)
 	@Convert(converter = NotificationTypeConverter.class)
 	private NotificationType notificationType;
+
+	@NotNull
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@NotNull
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	public static Notification from(CreateNotificationDto createNotificationDto) {
+		return Notification.builder()
+			.content(createNotificationDto.getContent())
+			.senderId(createNotificationDto.getSenderId())
+			.receiverId(createNotificationDto.getReceiverId())
+			.notificationType(NotificationType.ofName(createNotificationDto.getNotificationType()))
+			.createdAt(LocalDateTime.now())
+			.updatedAt(LocalDateTime.now())
+			.build();
+
+	}
 }
