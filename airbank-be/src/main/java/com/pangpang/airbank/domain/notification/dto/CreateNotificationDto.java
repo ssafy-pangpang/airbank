dbbdd4d1f0fa53e28640620f67e81af807dcb093
Chae Huichan <eqholic1125@gmail.com>
2023-09-21 23:15:40 +0900

    refactor: @JsonCreator, @JsonValue 사용으로 인한 Enum class 구조 변경 및 Dto 자료형 변경

--- airbank-be/src/main/java/com/pangpang/airbank/domain/notification/dto/CreateNotificationDto.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/notification/dto/CreateNotificationDto.java
@@ -1,5 +1,7 @@
 package com.pangpang.airbank.domain.notification.dto;
 
+import com.pangpang.airbank.global.meta.domain.NotificationType;
+
 import lombok.Getter;
 
 @Getter
@@ -7,5 +9,5 @@ public class CreateNotificationDto {
 	private String content;
 	private Long senderId;
 	private Long receiverId;
-	private String notificationType;
+	private NotificationType notificationType;
 }
--- airbank-be/src/main/java/com/pangpang/airbank/domain/notification/dto/NotificationElement.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/notification/dto/NotificationElement.java
@@ -3,6 +3,8 @@ package com.pangpang.airbank.domain.notification.dto;
 import java.time.LocalDateTime;
 
 import com.pangpang.airbank.domain.notification.domain.Notification;
+import com.pangpang.airbank.domain.notification.domain.NotificationGroup;
+import com.pangpang.airbank.global.meta.domain.NotificationType;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -12,14 +14,14 @@ import lombok.Getter;
 public class NotificationElement {
 	private String content;
 	private Boolean activated;
-	private String notificationType;
+	private NotificationType notificationType;
 	private LocalDateTime createdAt;
 
 	public static NotificationElement from(Notification notification) {
 		return NotificationElement.builder()
 			.content(notification.getContent())
 			.activated(notification.getActivated())
-			.notificationType(notification.getNotificationType().toString())
+			.notificationType(notification.getNotificationType())
 			.createdAt(notification.getCreatedAt())
 			.build();
 
