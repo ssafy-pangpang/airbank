8d3417a27814123f52a284b605901cce16eece00
Chae Huichan <eqholic1125@gmail.com>
2023-09-22 17:02:09 +0900

    feat: 계좌번호, 핀어카운트 조회 서비스 구현

--- airbank-be/src/main/java/com/pangpang/airbank/domain/account/repository/AccountRepository.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/account/repository/AccountRepository.java
@@ -1,10 +1,14 @@
 package com.pangpang.airbank.domain.account.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pangpang.airbank.domain.account.domain.Account;
+import com.pangpang.airbank.global.meta.domain.AccountType;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+	Optional<Account> findByMemberIdAndType(Long memberId, AccountType type);
 }
--- airbank-be/src/main/java/com/pangpang/airbank/domain/account/service/AccountService.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/account/service/AccountService.java
@@ -2,8 +2,13 @@ package com.pangpang.airbank.domain.account.service;
 
 import com.pangpang.airbank.domain.account.dto.CommonAccountIdResponseDto;
 import com.pangpang.airbank.domain.account.dto.PostEnrollAccountRequestDto;
+import com.pangpang.airbank.global.meta.domain.AccountType;
 
 public interface AccountService {
 	CommonAccountIdResponseDto saveAccount(PostEnrollAccountRequestDto postEnrollAccountRequestDto,
 		Long memberId);
+
+	String getAccountNumber(Long memberId, AccountType type);
+
+	String getFinAccountNumber(Long memberId, AccountType type);
 }
