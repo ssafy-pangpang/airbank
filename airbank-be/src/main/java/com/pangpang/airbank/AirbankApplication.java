7eb454c358a7350392d35ab8117e08a9e9d6e0d1
김민정 <minjeong.kim512@gmail.com>
2023-10-05 01:38:41 +0900

    Hotfix/change timezome
    * fix: 기준 시간 서울로 수정
    
    * comment: nh log 추가
    
    * fix: Loan 계좌 생성 에러 수정

--- airbank-be/src/main/java/com/pangpang/airbank/AirbankApplication.java
+++ airbank-be/src/main/java/com/pangpang/airbank/AirbankApplication.java
@@ -1,12 +1,21 @@
 package com.pangpang.airbank;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class AirbankApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirbankApplication.class, args);
