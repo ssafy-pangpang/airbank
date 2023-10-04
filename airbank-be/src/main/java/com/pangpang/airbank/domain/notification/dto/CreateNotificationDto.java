bd93a46505614956b35b7e97996680266f0ff776
김민정 <minjeong.kim512@gmail.com>
2023-10-04 16:42:30 +0900

    feat: 입금/출금에 알림 연동
    * feature: 입금/출금에 알림 연동

--- airbank-be/src/main/java/com/pangpang/airbank/domain/notification/dto/CreateNotificationDto.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/notification/dto/CreateNotificationDto.java
@@ -1,13 +1,40 @@
 package com.pangpang.airbank.domain.notification.dto;
 
+import com.pangpang.airbank.domain.account.dto.TransferRequestDto;
+import com.pangpang.airbank.domain.member.domain.Member;
 import com.pangpang.airbank.global.meta.domain.NotificationType;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CreateNotificationDto {
 	private String content;
 	private Long senderId;
 	private Long receiverId;
 	private NotificationType notificationType;
+
+	public static CreateNotificationDto from(TransferRequestDto transferRequestDto, String type) {
+		Member receiver = null, sender = null;
+		String msgType = "";
+
+		if (type.equals("withdraw")) {
+			receiver = transferRequestDto.getSenderAccount().getMember();
+			sender = transferRequestDto.getReceiverAccount().getMember();
+			msgType = "출금했습니다";
+		} else if (type.equals("deposit")) {
+			receiver = transferRequestDto.getReceiverAccount().getMember();
+			sender = transferRequestDto.getSenderAccount().getMember();
+			msgType = "입금받았습니다";
+		}
+
+		return CreateNotificationDto.builder()
+			.content(String.format("%s님에게 %s %s을 %s.", sender.getName(), transferRequestDto.getType().getMsgName(),
+				transferRequestDto.getAmount().toString().replaceAll("\\B(?=(\\d{3})+(?!\\d))", ","), msgType))
+			.senderId(sender.getId())
+			.receiverId(receiver.getId())
+			.notificationType(NotificationType.ofName(transferRequestDto.getType().getName()))
+			.build();
+	}
 }
